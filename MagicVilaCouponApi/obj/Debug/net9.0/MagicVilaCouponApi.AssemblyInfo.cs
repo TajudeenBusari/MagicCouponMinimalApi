29c274f36d973a23c9af8b224a6c7de412253f78
tajudeen busari <tajudeen.busari10@gmail.com>
2025-01-03 10:43:57 +0200

    first commit from feature branch

--- MagicVilaCouponApi/obj/Debug/net9.0/MagicVilaCouponApi.AssemblyInfo.cs
+++ MagicVilaCouponApi/obj/Debug/net9.0/MagicVilaCouponApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilaCouponApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c65f6f93265b51d2146d92e73d655d64014d3cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d299f55b0383e8fc29752817e1b086367810b515")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilaCouponApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilaCouponApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
